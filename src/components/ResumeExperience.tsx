5054e56451015d0e259a74c8beecc02f92f400df
Ben Adamsky <benadamsky@gmail.com>
2024-05-14 22:21:38 -0400

    quick cleanup

--- src/components/ResumeExperience.tsx
+++ src/components/ResumeExperience.tsx
@@ -19,187 +19,129 @@ const ExperienceItem: FC<ExperienceItemProps> = ({
   end,
   children,
   experienceItemClassName = 'mb-1'
-}) => {
-  return (
-    <div>
-      <h5 className={`text-2xl font-semibold ${experienceItemClassName}`}>
-        {role} — {company}
-      </h5>
-      {description && <p className="text-lg font-medium">{description}</p>}
-      <DateRange start={start} end={end} />
-      {children}
-    </div>
-  );
-};
+}) => (
+  <div>
+    <h5 className={`text-2xl font-semibold ${experienceItemClassName}`}>
+      {role} — {company}
+    </h5>
+    {description && <p className="text-lg font-medium">{description}</p>}
+    <DateRange start={start} end={end} />
+    {children}
+  </div>
+);
 
 interface ExperienceListProps {
-  children: ReactNode;
+  items: string[];
 }
 
-const ExperienceList: FC<ExperienceListProps> = ({ children }) => {
-  return <ul className="list-disc ml-4 mt-2 space-y-2">{children}</ul>;
-};
+const ExperienceList: FC<ExperienceListProps> = ({ items }) => (
+  <ul className="list-disc ml-4 mt-2 space-y-2">
+    {items.map((item, index) => (
+      <li key={index}>{item}</li>
+    ))}
+  </ul>
+);
 
-const ResumeExperience = () => {
-  return (
-    <div>
-      <h2 className="text-4xl font-semibold mb-3">Experience</h2>
-      <div className="space-y-8">
+const experiences = [
+  {
+    role: 'Co-Founder, CTO',
+    company: 'Ponder',
+    description: 'A web3 social survey platform',
+    start: 'Dec 2023',
+    end: 'Present',
+    items: []
+  },
+  {
+    role: 'Software Engineer',
+    company: 'Freeport',
+    description: 'A web3 platform dedicated to simplifying fine art investing',
+    start: 'Dec 2022',
+    end: 'Apr 2024',
+    items: [
+      'Core engineer in the successful launch of a collection of Warhols that generated over $150,000 in revenue within the first 6 weeks',
+      'Designed and delivered extensive platform features end-to-end, devised API routes and backend models, and optimized the analytics pipeline',
+      'Engineered sophisticated UI elements in a virtual art gallery that seamlessly integrated tokenized real-world assets and NFTs',
+      'Collaborated closely with the CEO and CTO to ideate and implement growth-oriented features, including high impact referral and collector programs'
+    ]
+  },
+  {
+    role: 'Freelance React Developer',
+    company: 'Upwork',
+    start: 'Dec 2018',
+    end: 'Dec 2022',
+    items: [
+      'Provided freelance services to clients ranging from startups to enterprise-level businesses',
+      'Achieved Top Rated status within the first year on the platform and Expert-Vetted shortly after, a distinction representing the top 1% of talent',
+      'Conceptualized and architected dashboards, onboarding flows, landing pages, API integrations, and fully responsive interfaces for web and native apps',
+      'Authored industry-spanning web and brand copy for businesses, including high profile clients such as Lyft and Carnival Cruise Line'
+    ]
+  },
+  {
+    role: 'Co-Founder, COO',
+    company: 'Kettle',
+    description: 'Online events built for communities',
+    start: 'May 2021',
+    end: 'Jul 2022',
+    items: [
+      'Oversaw product development from ideation to launch, guided MVP iterations, and coordinated user feedback to refine onboarding processes',
+      'Directly oversaw 4 software engineers in a rapid decision-making environment',
+      'Developed advanced virtual event software that supported real-time activities'
+    ]
+  },
+  {
+    role: 'Founding Software Engineer',
+    company: 'Branch',
+    description: 'An immersive virtual HQ for remote teams',
+    start: 'Apr 2020',
+    end: 'Jan 2021',
+    items: [
+      'Developed innovative peer-to-peer spatial audio solutions for virtual offices in a dynamic, venture-backed tech startup',
+      'Part of the core team responsible for an MVP that secured $15.5M in funding from investors such as Naval Ravikant, Sahil Lavingia, and Homebrew',
+      'Played a key role in making major architectural decisions of the core app and custom style library'
+    ]
+  },
+  {
+    role: 'Co-Founder, Director of Operations',
+    company: 'Konjure',
+    description: 'A decentralized website builder',
+    start: 'May 2018',
+    end: 'Dec 2019',
+    items: [
+      'Developed a peer-to-peer website builder that reached finalist status in multiple accelerator programs',
+      'Contributed to the tokenomics of the KONJ token and aided in the development of a desktop app for running validator nodes hosted on IPFS',
+      'Recruited for and represented the company at numerous blockchain events in NYC and SF'
+    ]
+  },
+  {
+    role: 'Web & Game Developer',
+    company: 'Self-Employed',
+    start: 'Feb 2014',
+    end: 'Sep 2016',
+    items: [
+      'Developed websites, configured game servers, and developed game plugins as side income while attending high school',
+      'Contributed to several bootstrapped game and web hosting companies, collectively serving thousands of users, later bought out by industry-leading competitors'
+    ]
+  }
+];
+
+const ResumeExperience: FC = () => (
+  <div>
+    <h2 className="text-4xl font-semibold mb-3">Experience</h2>
+    <div className="space-y-8">
+      {experiences.map(({ role, company, description, start, end, items }) => (
         <ExperienceItem
-          role="Co-Founder, CTO"
-          company="Ponder"
-          description="A web3 social survey platform"
-          start="Dec 2023"
-          end="Present"
+          key={role + company}
+          role={role}
+          company={company}
+          description={description}
+          start={start}
+          end={end}
         >
-          <div />
+          {items.length > 0 && <ExperienceList items={items} />}
         </ExperienceItem>
-        <ExperienceItem
-          role="Software Engineer"
-          company="Freeport"
-          description="A web3 platform dedicated to simplifying fine art investing"
-          start="Dec 2022"
-          end="Apr 2024"
-        >
-          <ExperienceList>
-            <li>
-              Core engineer in the successful launch of a collection of Warhols
-              that generated over $150,000 in revenue within the first 6 weeks
-            </li>
-            <li>
-              Designed and delivered extensive platform features end-to-end,
-              devised API routes and backend models, and optimized the analytics
-              pipeline
-            </li>
-            <li>
-              Engineered sophisticated UI elements in a virtual art gallery that
-              seamlessly integrated tokenized real-world assets and NFTs
-            </li>
-            <li>
-              Collaborated closely with the CEO and CTO to ideate and implement
-              growth-oriented features, including high impact referral and
-              collector programs
-            </li>
-          </ExperienceList>
-        </ExperienceItem>
-        <ExperienceItem
-          role="Freelance React Developer"
-          company="Upwork"
-          start="Dec 2018"
-          end="Dec 2022"
-        >
-          <ExperienceList>
-            <li>
-              Provided freelance services to clients ranging from startups to
-              enterprise-level businesses
-            </li>
-            <li>
-              Achieved Top Rated status within the first year on the platform
-              and Expert-Vetted shortly after, a distinction representing the
-              top 1% of talent
-            </li>
-            <li>
-              Conceptualized and architected dashboards, onboarding flows,
-              landing pages, API integrations, and fully responsive interfaces
-              for web and native apps
-            </li>
-            <li>
-              Authored industry-spanning web and brand copy for businesses,
-              including high profile clients such as Lyft and Carnival Cruise
-              Line
-            </li>
-          </ExperienceList>
-        </ExperienceItem>
-        <ExperienceItem
-          role="Co-Founder, COO"
-          company="Kettle"
-          description="Online events built for communities"
-          start="May 2021"
-          end="Jul 2022"
-        >
-          <ExperienceList>
-            <li>
-              Oversaw product development from ideation to launch, guided MVP
-              iterations, and coordinated user feedback to refine onboarding
-              processes
-            </li>
-            <li>
-              Directly oversaw 4 software engineers in a rapid decision making
-              environment
-            </li>
-            <li>
-              Developed advanced virtual event software that supported real-time
-              activities
-            </li>
-          </ExperienceList>
-        </ExperienceItem>
-        <ExperienceItem
-          role="Founding Software Engineer"
-          company="Branch"
-          description="An immersive virtual HQ for remote teams"
-          start="Apr 2020"
-          end="Jan 2021"
-        >
-          <ExperienceList>
-            <li>
-              Developed innovative peer-to-peer spatial audio solutions for
-              virtual offices in a dynamic, venture-backed tech startup
-            </li>
-            <li>
-              Part of the core team responsible for an MVP that secured $15.5M
-              in funding from investors such as Naval Ravikant, Sahil Lavingia,
-              and Homebrew
-            </li>
-            <li>
-              Played a key role in making major architectural decisions of the
-              core app and custom style library
-            </li>
-          </ExperienceList>
-        </ExperienceItem>
-        <ExperienceItem
-          role="Co-Founder, Director of Operations"
-          company="Konjure"
-          description="A decentralized website builder"
-          start="May 2018"
-          end="Dec 2019"
-        >
-          <ExperienceList>
-            <li>
-              Developed a peer-to-peer website builder that reached finalist
-              status in multiple accelerator programs
-            </li>
-            <li>
-              Contributed to the tokenomics of the KONJ token and aided in the
-              development of a desktop app for running validator nodes hosted on
-              IPFS
-            </li>
-            <li>
-              Recruited for and represented the company at numerous blockchain
-              events in NYC and SF
-            </li>
-          </ExperienceList>
-        </ExperienceItem>
-        <ExperienceItem
-          role="Web & Game Developer"
-          company="Self-Employed"
-          start="Feb 2014"
-          end="Sep 2016"
-        >
-          <ExperienceList>
-            <li>
-              Developed websites, configured game servers, and developed game
-              plugins as side income while attending high school
-            </li>
-            <li>
-              Contributed to several bootstrapped game and web hosting
-              companies, collectively serving thousands of users, later bought
-              out by industry-leading competitors
-            </li>
-          </ExperienceList>
-        </ExperienceItem>
-      </div>
+      ))}
     </div>
-  );
-};
+  </div>
+);
 
 export default ResumeExperience;
